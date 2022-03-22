5ff65c51ec6a96ec6f0e295b9a4135fc5a8c7e7f
matlevera <100791350+matlevera@users.noreply.github.com>
2022-03-22 09:36:50 -0400

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,7 @@
 int
 main()
 {
-  std::string name;
-  std::cin >> name;
-  std::cout << "hello world " << std::name << std::endl;
+  std::string name; // jvkhjvkhjv
+  std::cin >> name; // ljkhljl
+  std::cout << "hello world " << std::name << std::endl; // lkhhll
 }
